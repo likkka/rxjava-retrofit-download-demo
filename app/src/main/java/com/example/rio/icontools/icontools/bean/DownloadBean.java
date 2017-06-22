2e0433777b5c322c040c882f6c680c029e1110e7
黄敏芝 <huangminzhi@meizu.com>
2017-06-22 10:37:08 +0800

    init

---
+++ app/src/main/java/com/example/rio/icontools/icontools/bean/DownloadBean.java
@@ -0,0 +1,16 @@
+package com.example.rio.icontools.icontools.bean;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by rio on 17-6-21.
+ */
+
+public class DownloadBean {
+     public String pkgName;
+     public int launcherIconVersion;
+     public int statusBarIconVersion;
+     public Bitmap launcherIcon;
+     public Bitmap statusIcon;
+     public boolean isValid;
+}
---
+++ app/src/main/java/com/example/rio/icontools/icontools/utils/IconUtils.java
@@ -0,0 +1,19 @@
+package com.example.rio.icontools.icontools.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+
+/**
+ * Created by rio on 17-6-21.
+ */
+
+public class IconUtils {
+    private String FLYMEICON_PATH = "/sdcard/Customize/.FlymeIcon/";
+    //根据机型获取dpi
+    public static int getDpi(Context context) {
+        return context.getResources().getDisplayMetrics().densityDpi;
+    }
+    public static void saveBitmap(int type, String name, Bitmap bitmap) {
+
+    }
+}
